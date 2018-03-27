50c5d461c9ee167ef57dcd5317b9065a9bc5f1a3
Anton Arhipov <ant.arhipov@gmail.com>
2018-03-28 00:44:39 +0300

    testing partial commit

--- src/main/java/org/badger/Application.java
+++ src/main/java/org/badger/Application.java
@@ -1,6 +1,5 @@
 package org.badger;
 
-import org.jetbrains.annotations.Nullable;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
--- src/main/java/org/badger/Handler.java
+++ src/main/java/org/badger/Handler.java
@@ -2,14 +2,18 @@ package org.badger;
 
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Handler {
 
-  @RequestMapping("/hello")
+  @RequestMapping(value = "/hello", method = RequestMethod.GET)
   public String hello() {
-    //blah
+    return response();
+  }
+
+  private String response() {
     //language=HTML
     return "<html><body><h1>Hello!</h1></body></html>";
   }
