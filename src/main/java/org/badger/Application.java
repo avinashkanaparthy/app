0fc66b717a544e9fc2d52653408bb134d57a9f7a
Anton Arhipov <ant.arhipov@gmail.com>
2018-04-23 14:23:50 +0300

    trigger the build again

--- src/main/java/org/badger/Application.java
+++ src/main/java/org/badger/Application.java
@@ -8,7 +8,6 @@ import org.springframework.context.annotation.ComponentScan;
 @ComponentScan("org.badger")
 public class Application {
 
-  //trigger the build
   public static void main(String[] args) {
     SpringApplication.run(Application.class, args);
   }
