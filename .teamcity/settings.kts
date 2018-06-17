cc0ca54244c1f170e0c3fc6141ce098f14e51656
Anton Arhipov <ant.arhipov@gmail.com>
2018-06-18 01:35:24 +0300

    2017.2

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -3,7 +3,7 @@ import jetbrains.buildServer.configs.kotlin.v2018_1.buildSteps.gradle
 import jetbrains.buildServer.configs.kotlin.v2018_1.project
 import jetbrains.buildServer.configs.kotlin.v2018_1.version
 
-version = "2018.1"
+version = "2017.2"
 
 project {
     buildType(Build)
