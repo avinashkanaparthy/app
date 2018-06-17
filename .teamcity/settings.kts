a5fc35e914c057e36b83477ed630ca0326bacb9d
Anton Arhipov <ant.arhipov@gmail.com>
2018-06-18 01:45:02 +0300

    gradle settings

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,32 +1,12 @@
-import jetbrains.buildServer.configs.kotlin.v2018_1.*
+import jetbrains.buildServer.configs.kotlin.v2018_1.BuildType
 import jetbrains.buildServer.configs.kotlin.v2018_1.buildSteps.gradle
-
-/*
-The settings script is an entry point for defining a TeamCity
-project hierarchy. The script should contain a single call to the
-project() function with a Project instance or an init function as
-an argument.
-
-VcsRoots, BuildTypes, Templates, and subprojects can be
-registered inside the project using the vcsRoot(), buildType(),
-template(), and subProject() methods respectively.
-
-To debug settings scripts in command-line, run the
-
-    mvnDebug org.jetbrains.teamcity:teamcity-configs-maven-plugin:generate
-
-command and attach your debugger to the port 8000.
-
-To debug in IntelliJ Idea, open the 'Maven Projects' tool window (View
--> Tool Windows -> Maven Projects), find the generate task node
-(Plugins -> teamcity-configs -> teamcity-configs:generate), the
-'Debug' option is available in the context menu for the task.
-*/
+import jetbrains.buildServer.configs.kotlin.v2018_1.project
+import jetbrains.buildServer.configs.kotlin.v2018_1.triggers.vcs
+import jetbrains.buildServer.configs.kotlin.v2018_1.version
 
 version = "2017.2"
 
 project {
-
     buildType(Build)
 }
 
@@ -43,8 +23,12 @@ object Build : BuildType({
         gradle {
             tasks = "clean build"
             buildFile = ""
-            jdkHome = "%env.JDK_18%"
+            gradleWrapperPath = ""
         }
+    }
+
+    triggers {
+        vcs {  }
     }
 
     cleanup {
