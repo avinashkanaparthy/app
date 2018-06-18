e600f7ebbf8946f90e78b24f813226f84516f142
Anton Arhipov <ant.arhipov@gmail.com>
2018-06-18 19:56:33 +0300

    new build configuration

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -2,10 +2,56 @@ import jetbrains.buildServer.configs.kotlin.v2018_1.BuildType
 import jetbrains.buildServer.configs.kotlin.v2018_1.DslContext
 import jetbrains.buildServer.configs.kotlin.v2018_1.buildSteps.gradle
 import jetbrains.buildServer.configs.kotlin.v2018_1.project
+import jetbrains.buildServer.configs.kotlin.v2018_1.triggers.vcs
 import jetbrains.buildServer.configs.kotlin.v2018_1.version
 
 version = "2018.1"
 
 project {
-
+    buildType(Build)
+    buildType(Upload)
 }
+
+object Build : BuildType({
+    name = "Build"
+
+    artifactRules = "build/libs/app*.jar"
+
+    vcs {
+        root(DslContext.settingsRoot)
+    }
+
+    steps {
+        gradle {
+            tasks = "clean build"
+        }
+    }
+
+    cleanup {
+        history(days = 2)
+        artifacts(days = 2)
+    }
+})
+
+object Upload : BuildType({
+    name = "Upload"
+
+    dependencies {
+        dependency(Build) {
+            snapshot {  }
+        }
+
+        dependency(Build) {
+            artifacts {
+                artifactRules = "*.jar"
+                sameChainOrLastFinished()
+            }
+        }
+    }
+
+    triggers {
+        vcs {
+            watchChangesInDependencies = true
+        }
+    }
+})
