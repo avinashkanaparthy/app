2dbca10913c1fe3b0f4a2641abb800f2bff737b7
Anton Arhipov <ant.arhipov@gmail.com>
2018-06-19 00:46:59 +0300

    new build configuration

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -18,6 +18,8 @@ project {
 object Build : BuildType({
     name = "Build"
 
+    artifactRules = "build/libs/app*.jar"
+
     vcs {
         root(DslContext.settingsRoot)
     }
@@ -25,8 +27,6 @@ object Build : BuildType({
     steps {
         gradle { tasks = "clean build" }
     }
-
-    artifactRules = "build/libs/app*.jar"
 
     cleanup {
         artifacts(days = 2)
@@ -59,5 +59,5 @@ object Upload : BuildType({
 
     triggers { vcs {
         watchChangesInDependencies = true
-    } }
+    }}
 })
