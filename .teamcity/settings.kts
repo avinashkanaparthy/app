2367a31d46b266ca07f841f4d0c223d5d7b91f36
Anton Arhipov <ant.arhipov@gmail.com>
2018-06-18 19:29:26 +0300

    clean stateˆ

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -8,50 +8,5 @@ import jetbrains.buildServer.configs.kotlin.v2018_1.version
 version = "2018.1"
 
 project {
-    buildType(Build)
-    buildType(Upload)
+
 }
-
-object Build : BuildType({
-    name = "Build"
-
-    artifactRules = "build/libs/app*.jar"
-
-    vcs {
-        root(DslContext.settingsRoot)
-    }
-
-    steps {
-        gradle {
-            tasks = "clean build"
-        }
-    }
-
-    cleanup {
-        artifacts(days = 2)
-        history(days = 2)
-    }
-})
-
-object Upload : BuildType({
-    name = "Upload"
-
-    dependencies {
-        dependency(Build) {
-            snapshot { }
-        }
-
-        dependency(Build) {
-            artifacts {
-                artifactRules = "*.jar"
-                sameChainOrLastFinished()
-            }
-        }
-    }
-
-    triggers {
-        vcs {
-            watchChangesInDependencies = true
-        }
-    }
-})
