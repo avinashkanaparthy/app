a5512048a9ed42b19df563e8d58c66c442c3693e
Anton Arhipov <ant.arhipov@gmail.com>
2018-06-19 13:41:24 +0300

    new build configuration

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,7 +1,29 @@
-
+import jetbrains.buildServer.configs.kotlin.v2018_1.BuildType
+import jetbrains.buildServer.configs.kotlin.v2018_1.DslContext
+import jetbrains.buildServer.configs.kotlin.v2018_1.buildSteps.gradle
+import jetbrains.buildServer.configs.kotlin.v2018_1.buildSteps.script
+import jetbrains.buildServer.configs.kotlin.v2018_1.project
+import jetbrains.buildServer.configs.kotlin.v2018_1.triggers.vcs
+import jetbrains.buildServer.configs.kotlin.v2018_1.version
 
 version = "2018.1"
 
 project {
+    buildType {
+        id("Build")
+        name = "Build"
 
+        vcs { root(DslContext.settingsRoot) }
+
+        steps {
+            gradle { tasks = "clean build" }
+        }
+
+        artifactRules = "clean build"
+
+        cleanup {
+            artifacts(days = 2)
+            history(days = 2)
+        }
+    }
 }
