0542e221ca536d7e0fed16a171d0dd5993162da4
Anton Arhipov <ant.arhipov@gmail.com>
2018-07-09 22:05:51 +0300

    deployˆ

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,5 +1,6 @@
 import jetbrains.buildServer.configs.kotlin.v2018_1.*
 import jetbrains.buildServer.configs.kotlin.v2018_1.buildSteps.gradle
+import jetbrains.buildServer.configs.kotlin.v2018_1.buildSteps.script
 import jetbrains.buildServer.configs.kotlin.v2018_1.triggers.vcs
 
 /*
@@ -29,6 +30,7 @@ version = "2018.1"
 project {
 
     buildType(Build)
+    buildType(Deploy)
 }
 
 object Build : BuildType({
@@ -48,8 +50,31 @@ object Build : BuildType({
         }
     }
 
-    triggers {
-        vcs {
+
+})
+
+object Deploy : BuildType({
+    name = "Deploy"
+
+    steps {
+        script {
+            scriptContent = """
+                echo 'hello!'
+            """.trimIndent()
         }
     }
+
+    dependencies {
+        snapshot(Build){}
+        artifacts(Build){
+            artifactRules = "*.jar"
+        }
+    }
+
+    triggers {
+        vcs {
+            watchChangesInDependencies = true
+        }
+    }
+
 })
