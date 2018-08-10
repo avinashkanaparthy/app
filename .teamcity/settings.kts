75381373df30fc7430b4f7d6c5cb67c542384b50
Anton Arhipov <ant.arhipov@gmail.com>
2018-08-10 14:05:15 +0200

    small config

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,8 +1,27 @@
 import jetbrains.buildServer.configs.kotlin.v2018_1.*
+import jetbrains.buildServer.configs.kotlin.v2018_1.buildSteps.gradle
+import jetbrains.buildServer.configs.kotlin.v2018_1.buildSteps.script
 import jetbrains.buildServer.configs.kotlin.v2018_1.triggers.vcs
 
 version = "2018.1"
 
 project {
+
+    buildType {
+        id("Build")
+        name = "Gradle Build"
+
+        artifactRules = "build/app-*.jar"
+
+        steps {
+            gradle {
+                tasks = "clean build"
+            }
+        }
+
+        triggers {
+            vcs {}
+        }
+    }
 
 }
