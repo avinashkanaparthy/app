20d33d3eeda36cab32a4029bba6976b2440da982
admin <admin@TeamCity>
2018-08-27 15:21:58 +0300

    TeamCity change in 'App' project: Versioned settings configuration updated

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,21 +1,16 @@
 import jetbrains.buildServer.configs.kotlin.v2018_1.*
+import jetbrains.buildServer.configs.kotlin.v2018_1.buildSteps.gradle
+import jetbrains.buildServer.configs.kotlin.v2018_1.triggers.vcs
 
 /*
-The settings script is an entry point for defining a single
-TeamCity project. TeamCity looks for the 'settings.kts' file in a
-project directory and runs it if it's found, so the script name
-shouldn't be changed and its package should be the same as the
-project's id.
+The settings script is an entry point for defining a TeamCity
+project hierarchy. The script should contain a single call to the
+project() function with a Project instance or an init function as
+an argument.
 
-The script should contain a single call to the project() function
-with a Project instance or an init function as an argument.
-
-VcsRoots, BuildTypes, and Templates of this project must be
-registered inside project using the vcsRoot(), buildType(), and
-template() methods respectively.
-
-Subprojects can be defined either in their own settings.kts or by
-calling the subProjects() method in this project.
+VcsRoots, BuildTypes, Templates, and subprojects can be
+registered inside the project using the vcsRoot(), buildType(),
+template(), and subProject() methods respectively.
 
 To debug settings scripts in command-line, run the
 
@@ -23,11 +18,38 @@ To debug settings scripts in command-line, run the
 
 command and attach your debugger to the port 8000.
 
-To debug in IntelliJ Idea, open the 'Maven Projects' tool window (View ->
-Tool Windows -> Maven Projects), find the generate task
-node (Plugins -> teamcity-configs -> teamcity-configs:generate),
-the 'Debug' option is available in the context menu for the task.
+To debug in IntelliJ Idea, open the 'Maven Projects' tool window (View
+-> Tool Windows -> Maven Projects), find the generate task node
+(Plugins -> teamcity-configs -> teamcity-configs:generate), the
+'Debug' option is available in the context menu for the task.
 */
 
 version = "2018.1"
-project(_Self.Project)
+
+project {
+
+    buildType(Build)
+}
+
+object Build : BuildType({
+    name = "Build"
+
+    artifactRules = "build/libs/app-*.jar"
+
+    vcs {
+        root(DslContext.settingsRoot)
+    }
+
+    steps {
+        gradle {
+            tasks = "clean build"
+            buildFile = ""
+            gradleWrapperPath = ""
+        }
+    }
+
+    triggers {
+        vcs {
+        }
+    }
+})
