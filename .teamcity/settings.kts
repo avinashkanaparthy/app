e1516e5ce1d779113b003399353fd490951bd76f
Anton Arhipov <ant.arhipov@gmail.com>
2018-10-17 12:12:46 +0200

    trying out new abstractions

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -6,51 +6,71 @@ import jetbrains.buildServer.configs.kotlin.v2018_1.triggers.vcs
 version = "2018.1"
 
 project {
-    val app = buildType {
-        id("App")
-        name = "App"
-
-        steps {
-            
-        }
-
-        dependencies {
-        }
-
-        triggers {
-        }
-    }
-
-    val buildApplication = buildType {
-        id("BuildApplication")
-        name = "BuildApplication"
-
-        steps {
-        }
-
-        dependencies {
-        }
-
-        triggers {
-        }
-    }
-
-    val testApplication = buildType {
-        id("TestApplication")
-        name = "TestApplication"
-
-        type = BuildTypeSettings.Type.COMPOSITE
-
-        steps {
-        }
-
-
-        triggers {
+    pipeline {
+        phase("build stuff") {
+            +Build
+ //           +PrepareArtifacts
+            +Publish
         }
     }
 }
 
-object BuildMyApp : BuildType({
-    name = "BuildMyApp"
-
+object Build: BuildType({
+    id("Build")
+    name = "Build"
 })
+
+object PrepareArtifacts: BuildType({
+    id("PrepareArtifacts")
+    name = "PrepareArtifacts"
+})
+
+object Publish: BuildType({
+    id("Publish")
+    name = "Publish"
+})
+
+class Pipeline {
+    val phases = arrayListOf<Phase>()
+
+    fun phase(description: String = "", init: Phase.() -> Unit = {}) {
+        val newPhase = Phase()
+        newPhase.init()
+
+//        phases.lastOrNull()?.let { prevPhase ->
+//            newPhase.buildTypes.forEach {
+//                it.dependencies {
+//                    for (dependency in prevPhase.buildTypes) {
+//                        snapshot(it){}
+//                    }
+//                }
+//            }
+//        }
+        phases.add(newPhase)
+    }
+}
+
+class Phase {
+    val buildTypes = hashSetOf<BuildType>()
+
+    operator fun BuildType.unaryPlus() {
+        buildTypes.lastOrNull()?.let {
+            this.dependencies {
+                snapshot(it) {}
+            }
+        }
+
+        buildTypes.add(this)
+    }
+}
+
+fun Project.pipeline(init: Pipeline.() -> Unit = {}) {
+    val pipeline = Pipeline()
+    pipeline.init()
+    //register all builds in pipeline
+    pipeline.phases.forEach { phase ->
+        phase.buildTypes.forEach { bt ->
+            this.buildType(bt)
+        }
+    }
+}
