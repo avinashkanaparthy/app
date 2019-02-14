8beb9ed42f0bedec6c9bcd2413781f01c3f72477
Anton Arhipov <ant.arhipov@gmail.com>
2019-02-14 12:18:59 +0200

    blah

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,48 +1,86 @@
 import jetbrains.buildServer.configs.kotlin.v2018_2.*
+import jetbrains.buildServer.configs.kotlin.v2018_2.buildSteps.gradle
 import jetbrains.buildServer.configs.kotlin.v2018_2.buildSteps.maven
-
-/*
-The settings script is an entry point for defining a TeamCity
-project hierarchy. The script should contain a single call to the
-project() function with a Project instance or an init function as
-an argument.
-
-VcsRoots, BuildTypes, Templates, and subprojects can be
-registered inside the project using the vcsRoot(), buildType(),
-template(), and subProject() methods respectively.
-
-To debug settings scripts in command-line, run the
-
-    mvnDebug org.jetbrains.teamcity:teamcity-configs-maven-plugin:generate
-
-command and attach your debugger to the port 8000.
-
-To debug in IntelliJ Idea, open the 'Maven Projects' tool window (View
--> Tool Windows -> Maven Projects), find the generate task node
-(Plugins -> teamcity-configs -> teamcity-configs:generate), the
-'Debug' option is available in the context menu for the task.
-*/
+import jetbrains.buildServer.configs.kotlin.v2018_2.triggers.finishBuildTrigger
+import jetbrains.buildServer.configs.kotlin.v2018_2.vcs.GitVcsRoot
 
 version = "2018.2"
 
 project {
+    buildType {
+        id("Build")
+        name = "Build"
 
-    buildType(Build)
-}
+        vcs {
+            root(DslContext.settingsRoot)
+        }
 
-object Build : BuildType({
-    name = "Build"
+        artifactRules = "target/*.jar"
 
-    artifactRules = "target/*.jar"
+        steps {
+            gradle {
+                tasks = "clean build"
+            }
+        }
 
-    steps {
-        maven {
-            goals = "clean package"
+        cleanup {
+            artifacts(days = 3, builds = 50)
+            history(days = 5, builds = 100)
         }
     }
+}
 
-    cleanup {
-        history(builds = 100, days = 5)
-        artifacts(builds = 50, days = 3)
+
+//endregion
+
+//region pipeline
+fun Project.pipeline(init: Pipeline.() -> Unit = {}) {
+    val pipeline = Pipeline()
+    pipeline.init()
+
+    //register all builds in pipeline
+    pipeline.stages.forEach { it ->
+        it.buildTypes.forEach {
+            buildType(it)
+        }
     }
-})
+}
+
+@TeamCityDsl
+class Pipeline {
+    val stages = arrayListOf<Stage>()
+
+    fun stage(description: String, init: Stage.() -> Unit = {}) {
+        val newStage = Stage()
+        newStage.init()
+
+        stages.lastOrNull()?.let { prevStage ->
+            prevStage.buildTypes.lastOrNull()?.let { lastBuildType ->
+                newStage.buildTypes.firstOrNull()?.let {
+                    it.dependencies {
+                        snapshot(lastBuildType) {}
+                    }
+                }
+            }
+        }
+        stages.add(newStage)
+    }
+}
+//endregion
+
+//region phase
+class Stage {
+    val buildTypes = arrayListOf<BuildType>()
+
+    operator fun BuildType.unaryPlus() {
+        buildTypes.lastOrNull()?.let {
+            this.dependencies {
+//                snapshot(it) {}
+            }
+        }
+
+        buildTypes.add(this)
+    }
+}
+//endregion
+
