19989d22ed9f04fd4031c38a8040bcfdaa86b04c
Anton Arhipov <ant.arhipov@gmail.com>
2019-02-19 23:16:06 +0100

    experimenting with chains

--- .teamcity/dsllib.kt
+++ .teamcity/dsllib.kt
@@ -1,99 +1,107 @@
-//import jetbrains.buildServer.configs.kotlin.v2018_2.BuildType
-//import jetbrains.buildServer.configs.kotlin.v2018_2.Project
-//import jetbrains.buildServer.configs.kotlin.v2018_2.triggers.vcs
-//import java.lang.IllegalStateException
-//
-//class Chain {
-//    val stages = arrayListOf<Stage>()
-//}
-//
-//interface Stage
-//
-//class Serial(val buildType: BuildType) : Stage
-//
-//class Parallel: Stage {
-//    val buildTypes = arrayListOf<BuildType>()
-//}
-//
-//fun Parallel.build(bt: BuildType, block: BuildType.() -> Unit): BuildType {
-//    bt.apply(block)
-//    buildTypes.add(bt)
-//    return bt
-//}
-//
-//fun Chain.parallel(block: Parallel.() -> Unit): Stage {
-//    val parallel = Parallel().apply(block)
-//
-//    stages.lastOrNull()?.let { stage ->
-//        if(stage is Serial){
-//            parallel.buildTypes.forEach {
-//                it.dependencies.snapshot(stage.buildType){}
-//            }
-//        }
-//        if (stage is Parallel) {
-//            throw IllegalStateException("parallel cannot snapshot-depend on parallel")
-//        }
-//    }
-//
-//    stages.add(parallel)
-//    return parallel
-//}
-//
-//fun Chain.build(bt: BuildType, block: BuildType.() -> Unit): BuildType {
-//    bt.apply(block)
-//
-//    stages.lastOrNull()?.let { stage ->
-//        if(stage is Serial) {
-//            bt.dependencies.snapshot(stage.buildType){}
-//        }
-//        if (stage is Parallel) {
-//            stage.buildTypes.forEach {
-//                bt.dependencies.snapshot(it){}
-//            }
-//        }
-//    }
-//
-//    stages.add(Serial(bt))
-//    return bt
-//}
-//
-//fun Project.chain(block: Chain.() -> Unit): Chain {
-////    val chain = Chian()
-////    chain.block()
-//    val chain = Chain().apply(block)
-//
-//    //register all builds in pipeline
-//    chain.stages.forEach {
-//        if(it is Serial) {
-//            buildType(it.buildType)
-//        }
-//        if (it is Parallel) {
-//            it.buildTypes.forEach {
-//                buildType(it)
-//            }
-//        }
-//    }
-//
-//    chain.stages.lastOrNull()?.let {
-//        if (it is Serial) {
-//            it.buildType.triggers{
-//                vcs {
-//                    watchChangesInDependencies = true
-//                }
-//            }
-//        }
-//    }
-//
-//    return chain
-//}
-//
-//fun BuildType.produce(artifacts: String) {
-//    artifactRules = artifacts
-//}
-//
-//
-//fun BuildType.requires(bt: BuildType, artifacts: String) {
-//    dependencies.artifacts(bt) {
-//        artifactRules = artifacts
-//    }
-//}
+import jetbrains.buildServer.configs.kotlin.v2018_2.BuildType
+import jetbrains.buildServer.configs.kotlin.v2018_2.Project
+import java.lang.IllegalStateException
+
+class Chain {
+    val stages = arrayListOf<Stage>()
+}
+
+interface Stage
+
+class Serial(val buildType: BuildType) : Stage
+
+class Parallel: Stage {
+    val buildTypes = arrayListOf<BuildType>()
+    val chains = arrayListOf<Chain>()
+}
+
+fun Parallel.build(bt: BuildType, block: BuildType.() -> Unit): BuildType {
+    bt.apply(block)
+    buildTypes.add(bt)
+    return bt
+}
+
+fun Parallel.chain(block: Chain.()->Unit): Chain {
+    val chain = Chain().apply(block)
+    chains.add(chain)
+    return chain
+}
+
+fun Chain.parallel(block: Parallel.() -> Unit): Stage {
+    val parallel = Parallel().apply(block)
+
+    stages.lastOrNull()?.let { stage ->
+        if(stage is Serial){
+            parallel.buildTypes.forEach {
+                it.dependencies.snapshot(stage.buildType){}
+            }
+        }
+        if (stage is Parallel) {
+            throw IllegalStateException("paral lel cannot snapshot-depend on parallel")
+        }
+    }
+
+    stages.add(parallel)
+    return parallel
+}
+
+fun Chain.build(bt: BuildType, block: BuildType.() -> Unit): BuildType {
+    bt.apply(block)
+
+    registerDependency(bt, stages)
+
+    stages.add(Serial(bt))
+    return bt
+}
+
+private fun Chain.registerDependency(bt: BuildType, stages: List<Stage>) {
+    stages.lastOrNull()?.let { stage ->
+        if(stage is Serial) {
+            bt.dependencies.snapshot(stage.buildType){}
+        }
+        if (stage is Parallel) {
+            stage.buildTypes.forEach {
+                bt.dependencies.snapshot(it){}
+            }
+            stage.chains.forEach {
+                registerDependency(bt, it.stages)
+            }
+        }
+    }
+}
+
+fun Project.chain(block: Chain.() -> Unit): Chain {
+    val chain = Chain().apply(block)
+
+    //register all builds in pipeline
+    registerBuilds(chain)
+
+    return chain
+}
+
+private fun Project.registerBuilds(chain: Chain) {
+    chain.stages.forEach {
+        if (it is Serial) {
+            buildType(it.buildType)
+        }
+        if (it is Parallel) {
+            it.buildTypes.forEach {
+                buildType(it)
+            }
+            it.chains.forEach {
+                registerBuilds(it)
+            }
+        }
+    }
+}
+
+fun BuildType.produces(artifacts: String) {
+    artifactRules = artifacts
+}
+
+
+fun BuildType.requires(bt: BuildType, artifacts: String) {
+    dependencies.artifacts(bt) {
+        artifactRules = artifacts
+    }
+}
--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,57 +1,34 @@
 import jetbrains.buildServer.configs.kotlin.v2018_2.BuildType
-import jetbrains.buildServer.configs.kotlin.v2018_2.buildFeatures.swabra
 import jetbrains.buildServer.configs.kotlin.v2018_2.buildSteps.script
 import jetbrains.buildServer.configs.kotlin.v2018_2.project
 import jetbrains.buildServer.configs.kotlin.v2018_2.version
 
-
 /*
-The settings script is an entry point for defining a TeamCity
-project hierarchy. The script should contain a single call to the
-project() function with a Project instance or an init function as
-an argument.
-
-VcsRoots, BuildTypes, Templates, and subprojects can be
-registered inside the project using the vcsRoot(), buildType(),
-template(), and subProject() methods respectively.
-
-To debug settings scripts in command-line, run the
-
-    mvnDebug org.jetbrains.teamcity:teamcity-configs-maven-plugin:generate
-
-command and attach your debugger to the port 8000.
-
-To debug in IntelliJ Idea, open the 'Maven Projects' tool window (View
--> Tool Windows -> Maven Projects), find the generate task node
-(Plugins -> teamcity-configs -> teamcity-configs:generate), the
-'Debug' option is available in the context menu for the task.
-*/
+   chain    := (stage)+
+   stage    := (serial|parallel)+
+   parallel := (serial|chain)+
+   serial   := build
+ */
 
 version = "2018.2"
 
 project {
-    chain {
-        build(Compile) {
-            produces("application.jar")
-        }
+    val chain = chain {
+        build(Compile) {}
         parallel {
-            build(Test1) {
-                produces("test.reports.zip")
-                requires(Compile, "application.jar")
+            build(Test1) {}
+            chain {
+                build(Test2) {}
+                parallel {
+                    build(Test3) {}
+                    build(Test4) {}
+                }
             }
-            build(Test2) {
-                produces("test.reports.zip")
-                requires(Compile, "application.jar")
-            }
         }
-        build(Package) {
-            produces("application.zip")
-            requires(Compile, "application.jar")
-        }
-        build(Deploy) {
-            requires(Package, "application.zip")
-        }
+        build(Package) {}
+        build(Deploy) {}
     }
+    println(chain)
 }
 
 object Compile : BuildType({
@@ -84,6 +61,21 @@ object Test2 : BuildType({
             scriptContent = "touch test.reports.zip"
         }
     }
+})
+
+object Test3 : BuildType({
+    name = "Test3"
+
+    steps {
+        script {
+            scriptContent = "touch test.reports.zip"
+        }
+    }
+})
+
+object Test4 : BuildType({
+    name = "Test4"
+
 })
 
 object Package : BuildType({
